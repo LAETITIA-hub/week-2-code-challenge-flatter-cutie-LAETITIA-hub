e9de074beed0993c3df57421e3a225a23a9f05a6
LAETITIA-hub <laetitia.kamangu@student.moringaschool.com>
2025-03-23 12:07:29 +0300

    setting up the base url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const baseURL = "http://localhost:3000/characters";
+    const characterBar = document.getElementById("character-bar");
+    const detailedInfo = document.getElementById("detailed-info");
+    const nameDisplay = document.getElementById("name");
+    const imageDisplay = document.getElementById("image");
+    const voteCount = document.getElementById("vote-count");
+    const voteForm = document.getElementById("votes-form");
+    const voteInput = document.getElementById("votes");
+    const resetButton = document.getElementById("reset-btn");
+    const characterForm = document.getElementById("character-form");
+    const nameInput = document.getElementById("name");
+    const imageUrlInput = document.getElementById("image-url");
+
